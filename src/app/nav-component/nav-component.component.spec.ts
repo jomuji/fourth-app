bd59afea983adde24df66274fea7ad845496d44e
Jose Mujica <jomuji@gmail.com>
2016-08-24 17:52:28 -0400

    cmimit
    m

---
+++ src/app/nav-component/nav-component.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { NavComponentComponent } from './nav-component.component';
+
+describe('Component: NavComponent', () => {
+  it('should create an instance', () => {
+    let component = new NavComponentComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/nav-component/nav-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-nav-component',
+  templateUrl: 'nav-component.component.html',
+  styleUrls: ['nav-component.component.css']
+})
+export class NavComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
